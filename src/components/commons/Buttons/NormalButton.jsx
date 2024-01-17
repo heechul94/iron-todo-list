a25d114331addc39668570111c986379520913b5
heechul94 <yhcu3517@gmail.com>
2024-01-17 22:33:19 +0900

    :sparkles: Feat: 버튼 타입, onClick 속성 추가

--- src/components/commons/Buttons/NormalButton.jsx
+++ src/components/commons/Buttons/NormalButton.jsx
@@ -1,6 +1,10 @@
 import "./ButtonStyle.css";
 
-const NormalButton = ({ buttonName }) => {
-    return <button className="submit-button">{buttonName}</button>;
+const NormalButton = ({ type, buttonName, onClickAddTodo }) => {
+    return (
+        <button type={type} className="submit-button" onClick={onClickAddTodo}>
+            {buttonName}
+        </button>
+    );
 };
 export default NormalButton;
